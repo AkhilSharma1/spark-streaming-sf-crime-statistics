aba26914d8fd5a293207c90437103f7f0e5bcdb1
Akhil Sharma <akhil.sharma250@gmail.com>
2019-09-12 00:03:29 +0530

    add final changes

--- producer_server.py
+++ producer_server.py
@@ -4,6 +4,7 @@ import logging
 import pykafka
 import time
 
+KAFKA_SERVER = "localhost:9092"
 INPUT_FILE = pathlib.Path(__file__).parent.joinpath('resources/police-department-calls-for-service.json')
 
 logger = logging.getLogger(__name__)
@@ -26,13 +27,13 @@ def generate_data() -> None:
 def dict_to_binary(json_dict: dict) -> bytes:
     """
     Encode your json to utf-8
-    :param json_dict:
-    :return:
+    :param json_dict:json as python dict
+    :return: utf-8 encoded string
     """
     return json.dumps(json_dict).encode('utf-8')
 
 if __name__ == "__main__":
-    client = pykafka.KafkaClient("localhost:9092")
+    client = pykafka.KafkaClient(KAFKA_SERVER)
     print("topics", client.topics)
     producer = client.topics[b'service-calls'].get_producer()
 
